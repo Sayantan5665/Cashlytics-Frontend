d0e2d28db2dd98cc73621c7022d3747891887cf4
Sayantan Singha <sayantan.singha@webskitters.in>
2025-03-19 13:46:15 +0530

    file upload off

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -35,7 +35,7 @@ export class RegisterComponent {
   protected handleImageUpload(file: any) {
     const img: File = file.target.files[0];
     if (img) {
-      this.alert.toast("Please don't upload image as Vercel's ephemeral file system is read-only during runtime", 'error');
+      this.alert.toast("Please don't upload profile picture, this feature currently off as Vercel's ephemeral file system is read-only during runtime", 'error');
       return;
     }
     if (img && this.checkType(img)) {
